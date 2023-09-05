b72f4adcbcf0ed270a53721f2ee5a34311537182
Shuvonraj <125419101+Shuvonraj@users.noreply.github.com>
2023-09-06 02:11:06 +0600

    first commit

--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -4,26 +4,26 @@ import React from 'react';
 const Registration = () => {
   return (
     <>
-      <section className='flex justify-between overflow-x-hidden '>
-        <div className='lg:w-2/4 ml-[193px] mt-[103px]  lg:mt-[225px]  font-nunito '>
-          <div className='font-bold text-4xl'>Get started with easily register</div>
-          <div className='text-xl	pt-[13px] opacity-50 text-black font-normal'>Free register and you can enjoy it</div>
+      <section className='flex justify-between lg:mx-0 mx-auto text-center lg:text-left overflow-x-hidden '>
+        <div className='lg:w-2/4 lg:ml-[193px] mt-[99px]  lg:mt-[225px]  font-nunito '>
+          <div className='font-bold lg:px-0 px-5 text-2xl lg:text-4xl'>Get started with easily register</div>
+          <div className='lg:text-xl text-lg	pt-[13px] opacity-50 text-black font-normal'>Free register and you can enjoy it</div>
           <div className='mt-[62px] relative'>
-            <input placeholder='Email' className='border-2 focus:outline-none   w-96 py-6 px-14 border-[#11175D]  border-opacity-25 rounded-lg' type="text " />
-            <p className='absolute  top-[-12px] bg-[#ffffffff] px-3 left-6  '><span className='opacity-50 font-nunito tracking-widest'>Email Address</span> </p>
+            <input placeholder='Email' className='border-2 focus:outline-none   lg:w-96 py-6 px-14 border-[#11175D]  border-opacity-25 rounded-lg' type="text " />
+            <p className='absolute  top-[-12px] bg-[#ffffffff] px-3 lg:left-6 left-14  '><span className='opacity-50 font-nunito tracking-widest'>Email Address</span> </p>
           </div>
           <div className='mt-[56px] relative'>
-            <input placeholder='Your Name' className='border-2 focus:outline-none   w-96 py-6 px-14 border-[#11175D]  border-opacity-25 rounded-lg' type="text " />
-            <p className='absolute  top-[-12px] bg-[#ffffffff] px-3 left-6  '><span className='opacity-50 font-nunito tracking-widest'>Full Name</span> </p>
+            <input placeholder='Your Name' className='border-2 focus:outline-none   lg:w-96 py-6 px-14 border-[#11175D]  border-opacity-25 rounded-lg' type="text " />
+            <p className='absolute  top-[-12px] bg-[#ffffffff] px-3 lg:left-6 left-14  '><span className='opacity-50 font-nunito tracking-widest'>Full Name</span> </p>
           </div>
           <div className='mt-[56px] relative'>
-            <input placeholder='******' className='border-2 focus:outline-none   w-96 py-6 px-14 border-[#11175D]  border-opacity-25 rounded-lg' type="password" />
-            <p className='absolute  top-[-12px] bg-[#ffffffff] px-3 left-6  '><span className='opacity-50 font-nunito tracking-widest'>Password</span> </p>
+            <input placeholder='******' className='border-2 focus:outline-none   lg:w-96 py-6 px-14 border-[#11175D]  border-opacity-25 rounded-lg' type="password" />
+            <p className='absolute  top-[-12px] bg-[#ffffffff] px-3 lg:left-6 left-14 '><span className='opacity-50 font-nunito tracking-widest'>Password</span> </p>
           </div>
-          <button className='px-[158px]  w-96 font-nunito font-semibold text-white hover:bg-[#5f35f5a9] bg-[#5F35F5] py-5 mt-[52px] rounded-[86px] '>Sign up</button>
+          <button className='lg:px-[158px] px-[124px]  lg:w-96 font-nunito font-semibold text-white hover:bg-[#5f35f5a9] bg-[#5F35F5] py-5 mt-[52px] rounded-[86px] '>Sign up</button>
           
           
-          <p className=' font-nunito text-center w-96 mt-6 '>Already  have an account ? <span className=' font-nunito text-[#EA6C00] font-semibold '>Sign In</span></p>
+          <p className=' font-nunito text-center w-96 mt-6 pb-8 '>Already  have an account ? <span className=' font-nunito text-[#EA6C00] font-semibold '>Sign In</span></p>
         </div>
         
         
